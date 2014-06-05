c575e3ada5d7cfbcc82ac506bf3c8f06d649b76a
bougie <bougie@appartland.eu>
2014-06-05 21:18:25 +0200

    Issue #26 - You can include local pictures in a post

--- lib/static.py
+++ lib/static.py
@@ -1,16 +1,24 @@
 #-*- coding: utf8 -*-
 import os
 import shutil
+import logging
 
-def move_static_files(static_dir, output_dir):
-	if os.path.isdir(output_dir):
-		shutil.rmtree(output_dir)
+def move_files(input_dir, output_dir):
+	try:
+		if os.path.isdir(output_dir):
+			shutil.rmtree(output_dir)
 
-	if os.path.exists(static_dir):
-		items = os.listdir(static_dir)
-		for item in items:
-			tmpitem = os.path.join(static_dir, item)
-			if os.path.isdir(tmpitem):
-				shutil.copytree(tmpitem, os.path.join(output_dir, item))
-			else:
-				shutil.copy2(tmpitem, output_dir)
+		if os.path.exists(input_dir):
+			items = os.listdir(input_dir)
+			for item in items:
+				tmpitem = os.path.join(input_dir, item)
+				if os.path.isdir(tmpitem):
+					shutil.copytree(tmpitem, os.path.join(output_dir, item))
+				else:
+					try:
+						os.mkdir(output_dir)
+						shutil.copy2(tmpitem, output_dir)
+					except Exception as e:
+						logging.warning('Unable to copy files : %s' % (str(e)))
+	except Exception as e:
+		logging.warning('Unable to copy files reccursivly : %s' % (str(e)))
