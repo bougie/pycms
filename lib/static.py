5938fad3c4d00b45db1853d171518e36fdcc2f2c
bougie <bougie@appartland.eu>
2014-06-07 18:35:08 +0200

    We do not (re)create the output dir for each files in input dir

--- lib/static.py
+++ lib/static.py
@@ -9,6 +9,8 @@ def move_files(input_dir, output_dir):
 			shutil.rmtree(output_dir)
 
 		if os.path.exists(input_dir):
+			os.mkdir(output_dir)
+
 			items = os.listdir(input_dir)
 			for item in items:
 				tmpitem = os.path.join(input_dir, item)
@@ -16,7 +18,6 @@ def move_files(input_dir, output_dir):
 					shutil.copytree(tmpitem, os.path.join(output_dir, item))
 				else:
 					try:
-						os.mkdir(output_dir)
 						shutil.copy2(tmpitem, output_dir)
 					except Exception as e:
 						logging.warning('Unable to copy files : %s' % (str(e)))
